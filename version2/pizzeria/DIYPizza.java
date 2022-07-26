c313603f6e5009773306bc47f5b65080c677f486
Alvin Macion <macion.et.al@gmail.com>
2022-07-26 13:49:42 +0800

    Laboratory 4
    version2

---
+++ version2/pizzeria/DIYPizza.java
@@ -0,0 +1,33 @@
+gpackage pizzeria;
+import java.util.*;
+
+public class DIYPizza extends Pizza
+{
+	public DIYPizza(PizzaSize size)
+	{
+		super("DIY Pizza", size, PizzaBase.Original);
+	}
+
+	@Override
+	public double computePrice()
+	{
+		double totalCost = 0;
+
+		ArrayList<Topping> toppings = super.getToppings();
+
+		for(Topping t : toppings)
+			totalCost += t.getCost();
+
+		totalCost += 100;
+
+		switch(super.getSize())
+		{
+			case Double: totalCost *= 2;
+				break;
+			case Barkada: totalCost *= 5;
+				break;
+		}
+
+		return totalCost;
+	}
+}
