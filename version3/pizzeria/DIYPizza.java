71179fe0370065df383de9a196868513c29e8dba
KaelNierras <kyleanthony47@gmail.com>
2022-07-26 16:13:49 +0800

    update Kyle Nierras

--- version3/pizzeria/DIYPizza.java
+++ version3/pizzeria/DIYPizza.java
@@ -18,15 +18,16 @@ public class DIYPizza extends Pizza implements PricedItem
 
 		for(Topping t : toppings)
 			totalCost += t.getCost();
-			totalCost += 100;
-			switch (super.getSize())
-			{
-				case Double: totalCost *= 2; 
-							break;
-				case Barkada: totalCost *= 5; 
-							break;
+		
+		totalCost += 100;
+		switch (super.getSize())
+		{
+			case Double: totalCost *= 2; 
+						break;
+			case Barkada: totalCost *= 5; 
+						break;
 
-			}
+		}
 		return totalCost;
 	}
 
